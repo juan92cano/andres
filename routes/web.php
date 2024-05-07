eab34d3730076ee21012089083de77f19d3a29c1
juan92cano <juan92cano@gmail.com>
2024-05-07 11:44:04 -0600

    configuracion base de datos y login hecho con cuenta de prueba

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class, 'index'])->name('home');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/logados', [AuthController::class, 'logados'])->name('logados');
